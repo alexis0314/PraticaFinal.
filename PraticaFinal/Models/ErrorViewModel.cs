f0151373e452b7569f3540732f4feba925a69397
alexis0314 <alexismercedes365@gmail.com>
2025-07-12 15:18:37 -0400

    Creacion de los modelos

---
+++ PraticaFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PraticaFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
